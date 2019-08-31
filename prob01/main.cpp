42299bed7d815602d211d612c12870e53e5b7a7c
stsan <stsan@cs104u-1e.acad.fullerton.edu>
2019-08-31 13:31:33 -0700

    diamond added

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,18 @@
-// Name: 
+// Name: Steven Tsan
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
-
+std::cout << "```" << '\n';
+  std::cout << "    * " << '\n';
+  std::cout << "   *** " << '\n';
+  std::cout << "  ***** " << '\n';
+  std::cout << " ******* " << '\n';
+  std::cout << "  *****" << '\n';
+  std::cout << "   *** " << '\n';
+  std::cout << "    *  " << '\n';
+  std::cout << "```" << '\n';
   return 0;
 }
