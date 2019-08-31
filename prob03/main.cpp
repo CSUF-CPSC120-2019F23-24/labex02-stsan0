7c26eafb13b77d2b7b0bd9006e3d5281161b0be1
stsan <stsan@cs104u-1e.acad.fullerton.edu>
2019-08-31 14:29:24 -0700

    calculator created

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,2 +1,29 @@
-// Name: 
+// Name: Steven Tsan
 // This program calculates the tax and tip on a restaurant bill.
+#include <iostream>
+
+int main()
+
+{
+  double meal, tax, tip;
+  int total;
+std::cout << "```\n";
+std::cout << "Welcome to the Restaurant Bill Calculator.\n";
+  // Get the meal total.
+  std::cout << "What is the total meal cost?\n";
+  std::cin >> meal ;
+
+// Calculate for Tax and Tip, then add them to Meal for Bill.
+tax = meal * .0775;
+tip = meal * .2;
+total = meal + tax + tip;
+// Display total.
+
+std::cout << "Meal cost:" << meal << "$\n";
+std::cout << "Tax:" << tax << "$\n";
+std::cout << "Tip:" << tip << "$\n";
+std::cout << "Total Bill:" << total << "$\n";
+std::cout << "```";
+
+  return 0;
+}
